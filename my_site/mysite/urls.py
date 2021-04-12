d2af0494a5fd62b43e83b75147db885e54479a08
NCM <neenuchacko@live.in>
2021-04-12 12:23:09 +0530

    Before fork

--- my_site/mysite/urls.py
+++ my_site/mysite/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('authenticate.urls')),
     path('post/', include('post.urls')),
+    path('', include('post.urls')), # added it here just to deploy
     path('chatroom/', include('chat.urls')),
 ]
