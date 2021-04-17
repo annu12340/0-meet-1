1af1e1bbe0b8f98b97d91666d03859452ac60904
NCM <neenuchacko@live.in>
2021-04-18 02:29:52 +0530

    authenticate

--- mysite/urls.py
+++ mysite/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('authenticate.urls')),
     path('post/', include('post.urls')),
-    path('', include('post.urls')), # added it here just to deploy
+    path('', include('authenticate.urls')), # added it here just to deploy
     path('chatroom/', include('chat.urls')),
 
 
