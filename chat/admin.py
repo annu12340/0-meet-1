4044095a7e61c34103212bb9e6ac641df9bc9d4e
NCM <neenuchacko@live.in>
2021-04-23 23:33:32 +0530

    Configured admin a bit

--- chat/admin.py
+++ chat/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import messageStore
 # Register your models here.
-admin.site.register(messageStore)
+
+class messageAdmin(admin.ModelAdmin):
+    list_display = ('msg','sender','reciever')
+
+admin.site.register(messageStore,messageAdmin)
--- post/admin.py
+++ post/admin.py
@@ -4,6 +4,15 @@ from .models import EventsIdeaModel
 
 # Register your models here.
 # reg to admin panel
+class eventAdmin(admin.ModelAdmin):
+    list_display = ('EventName', 'EventDate', 'EventType')
+    list_filter = ('EventDate',)
 
-admin.site.register(PostIdeaModel)
-admin.site.register(EventsIdeaModel)
+
+class postAdmin(admin.ModelAdmin):
+    list_display = ('Title', 'Description', 'Progress')
+    list_filter = ('Title',)
+
+
+admin.site.register(PostIdeaModel,postAdmin)
+admin.site.register(EventsIdeaModel,eventAdmin)
